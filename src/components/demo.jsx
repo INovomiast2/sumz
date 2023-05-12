aebe0953507650dd0ebd68e2c809492b739c7f94
INovomiast-IESFuengirola1 <116105316+INovomiast-IESFuengirola1@users.noreply.github.com>
2023-05-12 02:09:33 +0200

    Cambiado un typo

--- src/components/demo.jsx
+++ src/components/demo.jsx
@@ -84,7 +84,7 @@ const Demo = () => {
           Article.summary && (
             <div className="flex flex-col gap-3">
               <h2 className="font-satoshi font-bold text-gray-600 text-xl">
-                Article <span className="blue_gradient">Summary</span>
+                Articulo <span className="blue_gradient">Resumido</span>
               </h2>
               <div className="summary_box">
                 <p className="font-inter font-medium text-sm text-gray-700">

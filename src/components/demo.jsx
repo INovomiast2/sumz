2798c6de583bba64e677739a32bbd6ba2844c295
INovomiast-IESFuengirola1 <116105316+INovomiast-IESFuengirola1@users.noreply.github.com>
2023-05-12 02:53:41 +0200

    Added Trash Icon and fixed some more typos

--- src/components/demo.jsx
+++ src/components/demo.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { copy, linkIcon, loader, tick } from '../assets';
+import { copy, linkIcon, loader, tick, trash } from '../assets';
 import { useLazyGetSummaryQuery } from "../services/article";
 
 const Demo = () => {
@@ -39,15 +39,14 @@ const Demo = () => {
 
       localStorage.setItem('articles', JSON.stringify(updatedAllArticles));
     }
-
   }
 
   return (
-    <section className="mt-16 w-full max-w-xl">
+    <section className="w-full max-w-xl mt-16">
       { /* Search */ }
       <div className="flex flex-col w-full gap-2">
-        <form className="relative flex justify-center items-center" onSubmit={handleSubmit}>
-          <img src={linkIcon} alt="link_icon" className="absolute left-0 my-2 ml-3 w-5" draggable="false"/>
+        <form className="relative flex items-center justify-center" onSubmit={handleSubmit}>
+          <img src={linkIcon} alt="link_icon" className="absolute left-0 w-5 my-2 ml-3" draggable="false"/>
           <input type="url" placeholder="Escribe una URL..." value={Article.url} onChange={(e) => setArticle({ ...Article, url: e.target.value })} required className="url_input peer" />
           <button type="submit" className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700">
             ➤
@@ -55,44 +54,47 @@ const Demo = () => {
         </form>
 
         {/* URL History */}
-        <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
+        <div className="flex flex-col gap-1 overflow-y-auto max-h-60">
           {allArticles.map((item, index) => (
             <div key={`link-${index}`} onClick={() => setArticle(item)} className="link_card">
               <div className="copy_btn">
                 <img src={copy} alt="copy_icon" className="w-[40%] h-[40%] object-contain" />
               </div>
-              <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate">
+              <p className="flex-1 text-sm font-medium text-blue-700 truncate font-satoshi">
                 {item.url}
               </p>
+              <div className="w-10">
+                <img src={trash} alt="trash_icon" className="w-[40%] h-[40%] object-contain hover:text-red-600" />
+              </div>
             </div>
           ))}
         </div>
       </div>
       {/* Result Display */}
-      <div className="my-10 max-w-full flex justify-center items-center">
+      <div className="flex items-center justify-center max-w-full my-10">
         {isFetching ? (
           <>
-            <img src={loader} alt="loader" className="w-20 h-20 object-contain" />
+            <img src={loader} alt="loader" className="object-contain w-20 h-20" />
             <br />
             <br />
-            <span className="text-lg text-gray-700 text-center">El Articulo está siendo <span className="grad_one">Resumido</span>...</span>
+            <span className="text-lg text-center text-gray-700">El Articulo está siendo <span className="grad_one">Resumido</span>...</span>
           </>
         ) : error ? (
-          <p className="font-inter font-bold text-black text-center">
+          <p className="font-bold text-center text-black font-inter">
             Bueno, eso no deberia haber pasado...
             <br />
-            <span className="font-satoshi font-normal text-gray-700">
+            <span className="font-normal text-gray-700 font-satoshi">
               {error ?.data?.error}
             </span>
           </p>
         ) : (
           Article.summary && (
             <div className="flex flex-col gap-3">
-              <h2 className="font-satoshi font-bold text-gray-600 text-xl">
+              <h2 className="text-xl font-bold text-gray-600 font-satoshi">
                 Articulo <span className="blue_gradient">Resumido</span>
               </h2>
               <div className="summary_box">
-                <p className="font-inter font-medium text-sm text-gray-700">
+                <p className="text-sm font-medium text-gray-700 font-inter">
                   {Article.summary}
                 </p>
               </div>

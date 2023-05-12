bf0074e4859ecbf1dc5e590dc8c4f81021094422
INovomiast-IESFuengirola1 <116105316+INovomiast-IESFuengirola1@users.noreply.github.com>
2023-05-12 02:07:01 +0200

    Last changes to Sumz

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
     <header className="w-full flex justify-center items-center flex-col">
       <nav className="flex justify-between items-center w-full mb-10 pt-3">
         <img src={logo} alt="sumz_logo" className="w-28 object-contain" draggable="false"/>
-        <button type="button" onClick={() => window.open('#')} className="black_btn">
+        <button type="button" onClick={() => window.open('https://github.com/INovomiast-IESFuengirola1/sumz')} className="black_btn">
           GitHub
         </button>
       </nav>
@@ -18,7 +18,7 @@ const Hero = () => {
         </span>
       </h1>
       <h2 className="desc">
-        Simplifica tu lectura con Sumarize, una herramienta
+        Simplifica tu lectura con <span className="grad_one">Sumz</span>, una herramienta
         Open-Source que con la tecnología de GPT-4 te resume
         cualquier artículo largo y pesado en un resumen claro
         y conciso.
